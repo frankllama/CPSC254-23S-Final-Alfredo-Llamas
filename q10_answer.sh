cff39fd74ae49329e9ff99bdf1921ce75bc04ff1
fllamas <chewfasa5@hotmail.com>
2023-05-16 17:22:19 -0700

    Adjusted file path for creating files and directories to be more universal.

--- q10_answer.sh
+++ q10_answer.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-mkdir -p /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src
-mkdir -p /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data
-mkdir -p /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/main
-mkdir -p /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/backup
-mkdir -p /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/config
+mkdir -p ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src
+mkdir -p ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data
+mkdir -p ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/main
+mkdir -p ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/backup
+mkdir -p ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/config
 
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src/main.cpp
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src/header.h
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src/main.cpp
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/src/header.h
 
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/main/mainDB.db
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/backup/backupDB.db
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/main/mainDB.db
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/data/backup/backupDB.db
 
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/config/config.yml
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/config/config.yml
 
-touch /home/chewfasa/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/README.MD
+touch ~/Spring23Link/CPSC\ 254/Final\ Exam/CPSC254-23S-Final-Alfredo-Llamas/final/README.MD
